ca7cbf8aa2d116891ddecf6bb8bac571d7a917ed
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-12-24 14:49:49 -0500

    complete phase 3, create show and create controller actions

--- practice_project/app/controllers/users_controller.rb
+++ practice_project/app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:create]
+
     def index 
         render plain: "I'm in the index action!"
+    end
+
+    def create
+        render json: params
+    end
+
+    def show 
+        render json: params
     end
 end
