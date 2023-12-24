7a7ae501549d9a95985ed838a4dce7dcbe7eca77
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-12-23 23:59:22 -0500

    edit user routes

--- practice_project/config/routes.rb
+++ practice_project/config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # resources :users
-  get "/users", to: "users#index", as: "users"
-  get "/users/:id", to: "users#show", as: "user"
+  resources :users
+    get "/users", to: "users#index", as: "users"
+    post "/users", to: "users#create", as: "create_user"
+    get "/users/new", to: "users#new", as: "new_user"
+    get "/users/:id/edit", to: "users#edit", as: "edit_user"
+    get "/users/:id", to: "users#show", as: "user"
+    patch "/users/:id", to: "users#update"
+    put "/users/:id", to: "users#update"
+    delete "/users/:id", to: "users#destroy" 
 end
