4841079f45d9046a2e2e60411c4b9ded451b9230
Yosyp Dobosh <joseph.dobosh@gmail.com>
2024-01-03 17:32:17 -0500

    fix index redundency in artworks table

--- practice_project/app/models/artwork.rb
+++ practice_project/app/models/artwork.rb
@@ -6,7 +6,7 @@ class Artwork < ApplicationRecord
     validates :user_id, presence: true
 
     # Associations
-    belongs_to :artist, class_name: 'User', foreign_key: "artist_id"
+    belongs_to :artist, class_name: 'User', foreign_key: 'artist_id'
     has_many :artwork_shares, dependent: :destroy
     has_many :shared_viewers, through: :artwork_shares, source: :viewer
 
--- practice_project/db/schema.rb
+++ practice_project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_01_023106) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_03_223112) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,7 +30,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_01_023106) do
     t.integer "artist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["artist_id"], name: "index_artworks_on_artist_id"
     t.index ["image_url"], name: "index_artworks_on_image_url", unique: true
     t.index ["title", "artist_id"], name: "index_artworks_on_title_and_artist_id", unique: true
   end
