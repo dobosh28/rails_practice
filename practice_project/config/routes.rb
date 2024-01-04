f6d7e826eb2f70a97cf90a958d474b4947a400c8
Yosyp Dobosh <joseph.dobosh@gmail.com>
2024-01-03 21:16:19 -0500

    create artwork routes and controller

--- practice_project/config/routes.rb
+++ practice_project/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index, :show, :update, :create, :destroy]
-
+  resources :artworks, only: [:index, :show, :update, :create, :destroy]
   # get "/users", to: "users#index", as: "users"
   # post "/users", to: "users#create"
   # get "/users/new", to: "users#new", as: "new_user"
