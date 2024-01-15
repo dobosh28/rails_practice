ea7c14424b2933dd6ae5ba47c49c2b2978269888
Yosyp Dobosh <joseph.dobosh@gmail.com>
2024-01-14 22:52:55 -0500

    fix some stuff in the models

--- practice_project/app/models/artwork.rb
+++ practice_project/app/models/artwork.rb
@@ -1,13 +1,12 @@
 class Artwork < ApplicationRecord
     # Validations
-
     validates :title, presence: true, uniqueness: { scope: :artist_id, message: "Artist cannot have two artworks with the same title" }
     validates :image_url, presence: true, uniqueness: true
-    validates :user_id, presence: true
-
+    validates :artist_id, presence: true  # Updated field name
+  
     # Associations
     belongs_to :artist, class_name: 'User', foreign_key: 'artist_id', inverse_of: :artworks
     has_many :artwork_shares, dependent: :destroy, inverse_of: :artwork
-    has_many :shared_viewers, through: :artwork_shares, source: :viewer, dependent: :destroy, inverse_of: :shared_artworks
-
+    has_many :shared_viewers, through: :artwork_shares, source: :viewer, inverse_of: :shared_artworks
 end
+  
--- practice_project/app/models/user.rb
+++ practice_project/app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
     # Associations
     has_many :artworks, foreign_key: 'artist_id', dependent: :destroy, inverse_of: :artist
     has_many :artwork_shares, foreign_key: :viewer_id, dependent: :destroy, inverse_of: :viewer
-    has_many :shared_artworks, through: :artwork_shares, source: :artwork, dependent: :destroy, inverse_of: :shared_viewers
+    has_many :shared_artworks, through: :artwork_shares, source: :artwork, inverse_of: :shared_viewers
 end
